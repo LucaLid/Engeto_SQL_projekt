fa4f0826083c1d6b24fe96b1c3d00e210dcf0184
LucaLid <155613068+LucaLid@users.noreply.github.com>
2024-08-24 11:53:02 +0200

    Create secondary_table.sql
    popis sekundární tabulky, vložení kódu pro její vytvoření

---
+++ secondary_table.sql
@@ -0,0 +1,11 @@
+--  Vytvoření tabulky pro srovnání s evropskými zeměmi, sledované období 2006-2018 (dle dostupnosti dat ke mzdám a cenám potravin) --  
+CREATE OR REPLACE TABLE t_lucie_lidajova_project_sql_secondary_final AS
+SELECT 
+	c.country, 
+	e.year, 
+	e.gdp, 
+	e.gini
+FROM countries c 
+JOIN economies e ON e.country = c.country
+WHERE c.continent = "Europe" AND e.YEAR BETWEEN 2006 AND 2018     -- omezeny roky; v datových sadách ke mzdám a cenám potravin jsou uvedena data z období 2006-2018 
+GROUP BY e.year, c.country; 
