0e0df36276d24ede3fe02ea429b9368a0306c6dc
LucaLid <155613068+LucaLid@users.noreply.github.com>
2024-08-25 12:19:59 +0200

    Update secondary_table.sql
    doplněn sloupec population

--- secondary_table.sql
+++ secondary_table.sql
@@ -3,9 +3,12 @@ CREATE OR REPLACE TABLE t_lucie_lidajova_project_sql_secondary_final AS
 SELECT 
 	c.country, 
 	e.year, 
+	c.population,
 	e.gdp, 
 	e.gini
 FROM countries c 
 JOIN economies e ON e.country = c.country
 WHERE c.continent = "Europe" AND e.YEAR BETWEEN 2006 AND 2018     -- omezeny roky; v datových sadách ke mzdám a cenám potravin jsou uvedena data z období 2006-2018 
 GROUP BY e.year, c.country; 
+
+SELECT * FROM t_lucie_lidajova_project_sql_secondary_final
