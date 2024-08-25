5ee4a97eb69b5906eac1c83e6925c92e3d7c707d
LucaLid <155613068+LucaLid@users.noreply.github.com>
2024-08-25 11:32:51 +0200

    Update Q3: Nejpomalejší zdražování.sql
    finální verze kódu s využitím WITH, v předchozích variantách jsem byl nějak zamotaná

--- Q3: Nejpomalejší zdražování.sql
+++ Q3: Nejpomalejší zdražování.sql
@@ -1,61 +1,35 @@
 Q3: Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)?
-CREATE OR REPLACE VIEW v_food_price_growth AS 
-WITH cte_price_growth AS (
-    SELECT
+WITH cte_q3 AS (  
+    SELECT 
         payroll_year,
         food_name,
-        AVG(food_price) AS average_food_price,
-        LAG(AVG(food_price), 1) OVER (PARTITION BY food_name ORDER BY payroll_year ASC) AS previous_year_average_price,
-        CASE 
-            WHEN LAG(AVG(food_price), 1) OVER (PARTITION BY food_name ORDER BY payroll_year ASC) IS NOT NULL THEN
-                (AVG(food_price) - LAG(AVG(food_price), 1) OVER (PARTITION BY food_name ORDER BY payroll_year ASC)) / LAG(AVG(food_price), 1) OVER (PARTITION BY food_name ORDER BY payroll_year ASC) * 100
-            ELSE NULL
-        END AS year_over_year_growth
+        AVG(food_price) AS average_price
     FROM 
-        t_Lucie_Lidajova_project_SQL_primary_final
-    GROUP BY
+        t_lucie_lidajova_project_sql_primary_final
+    GROUP BY 
         payroll_year,
         food_name
-)
-SELECT 
-    food_name,
-    payroll_year,
-    average_food_price,
-    previous_year_average_price,
-    year_over_year_growth
-FROM 
-    cte_price_growth
-ORDER BY 
-    food_name,
-    payroll_year;
-CREATE OR REPLACE VIEW v_food_price_min_growth AS 
-WITH cte_price_growth AS (
-    SELECT
-        payroll_year,
-        food_name,
-        AVG(food_price) AS average_food_price,
-        LAG(AVG(food_price), 1) OVER (PARTITION BY food_name ORDER BY payroll_year ASC) AS previous_year_average_price,
-        CASE 
-            WHEN LAG(AVG(food_price), 1) OVER (PARTITION BY food_name ORDER BY payroll_year ASC) IS NOT NULL THEN
-                (AVG(food_price) - LAG(AVG(food_price), 1) OVER (PARTITION BY food_name ORDER BY payroll_year ASC)) / LAG(AVG(food_price), 1) OVER (PARTITION BY food_name ORDER BY payroll_year ASC) * 100
-            ELSE 
-                NULL
-        END AS year_over_year_growth
+),
+cte2_q3 AS (
+    SELECT 
+        *,
+        LAG(average_price, 1) OVER (PARTITION BY food_name ORDER BY payroll_year ASC) AS previous_price
     FROM 
-        t_Lucie_Lidajova_project_SQL_primary_final
-    GROUP BY
-        payroll_year,
-        food_name
+        cte_q3
+),
+cte3_q3 AS (
+    SELECT 
+        *,
+        ROUND(((average_price - previous_price) / previous_price) * 100, 2) AS perc_growth
+    FROM 
+        cte2_q3
 )
-SELECT 
+SELECT
     food_name,
-    year_over_year_growth
+    ROUND(AVG(perc_growth), 2) AS perc_growth_final
 FROM 
-    cte_price_growth
-WHERE 
-    year_over_year_growth IS NOT NULL
+    cte3_q3
+GROUP BY 
+    food_name 
 ORDER BY 
-    year_over_year_growth ASC
-
-SELECT * FROM v_food_price_min_growth 
-GROUP BY food_name
+    perc_growth_final ASC;
