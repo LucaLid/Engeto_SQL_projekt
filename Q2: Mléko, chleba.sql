88e389260ad6e2e2cfd682c0667e64375225c6a2
LucaLid <155613068+LucaLid@users.noreply.github.com>
2024-08-25 10:18:06 +0200

    Update Q2: Mléko, chleba.sql
    úprava kódu

--- Q2: Mléko, chleba.sql
+++ Q2: Mléko, chleba.sql
@@ -1,13 +1,11 @@
 -- Q2: Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
 SELECT 
 	payroll_year ,
-	average_payroll, 
+	Round(avg(average_payroll)) AS avg_payroll, 
 	food_name,
 	food_price, 
-	round (average_payroll/food_price) AS quantity
+	round (AVG(average_payroll)/food_price) AS quantity
 	FROM t_lucie_lidajova_project_sql_primary_final tllpspf     
 		WHERE food_name IN ('Chléb konzumní kmínový','Mléko polotučné pasterované') 
 		AND payroll_year IN (2006,2018)
-GROUP BY food_name, 
-		 payroll_year ;   
-		
+GROUP BY food_name, payroll_year ;
